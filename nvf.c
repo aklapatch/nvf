526bd0d84a14a77f1c53874dab19744504b33675
Aaron Klapatch <klapatchaaron@gmail.com>
2024-05-25 20:47:11 +0000

    Added some functions for initializing basics

--- nvf.c
+++ nvf.c
@@ -1,1 +1,29 @@
 #include "nvf.h"
+
+
+/* Format example:
+   # This is a comment
+   #[ This is a multiline comment.
+      It takes up more than one line. ]#
+   int_ex 3
+   float_ex 5.7
+   str_ex "whee"
+   multiline_str_ex "line one"
+                    "line two"
+   other_str_ex |line one again
+                |line two again
+   BLOB_ex Bx010203040506070809
+   array_ex [ 1 2 "elem" ]
+   map_ex {
+   	key1 "val1"
+	key2 "val2"
+   }
+*/
+   
+nvf_alloc nvf_alloc_init(realloc_fn realloc_inst, free_fn free_inst) {
+	nvf_alloc r = {
+		.realloc_inst = realloc_inst,
+		.free_inst = free_inst,
+	};
+	return r;
+}
--- nvf.h
+++ nvf.h
@@ -1,2 +1,53 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
 
+typedef enum {
+	NVF_OK,
+	NVF_ERROR,
+	NVF_ALLOC_FAIL,
+	NVF_NUM_ERRS,
+} nvf_err;
 
+typedef enum {
+	NVF_FLOAT,
+	NVF_INT,
+	NVF_BLOB,
+	NVF_STRING,
+	NVF_MAP,
+	NVF_ARRAY,
+} nvf_data_type;
+
+typedef union nvf_value {
+	int64_t v_int;
+	double v_float;
+	char *v_string;
+} nvf_value;
+
+typedef struct nvf_array {
+	uintptr_t num;
+	// I can't think of a way to nest maps in an array.
+	// Don't allow that for now.
+	union { 
+		nvf_value val;
+		struct nvf_array *array;
+	} data[];
+} nvf_array;
+
+typedef struct nvf_map {
+	uintptr_t num;
+	char **keys;
+	union {
+		nvf_value val;
+		struct nvf_map *map;
+		struct nvf_array *array;
+	} *values;
+} nvf_map;
+
+typedef void* (*realloc_fn)(void *, size_t);
+typedef void (*free_fn)(void *);
+
+typedef struct nvf_alloc {
+	realloc_fn realloc_inst;
+	free_fn free_inst;
+} nvf_alloc;
