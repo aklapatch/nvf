277b966f15f8a32bfd2e74f3b279aad96e685992
Aaron Klapatch <klapatchaaron@gmail.com>
2024-05-26 03:46:41 +0000

    did first draft of int parsing

--- nvf.c
+++ nvf.c
@@ -1,6 +1,7 @@
 #include "nvf.h"
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 
 /* Format example:
@@ -26,18 +27,12 @@ nvf_root nvf_root_init(realloc_fn realloc_inst, free_fn free_inst) {
 	nvf_root r = {
 		.realloc_inst = realloc_inst,
 		.free_inst = free_inst,
-		.base_map = {0},
 	};
 	return r;
 }
 
 nvf_root nvf_root_default_init(void) {
-	nvf_root r = {
-		.realloc_inst = realloc,
-		.free_inst = free,
-		.base_map = {0},
-	};
-	return r;
+	return nvf_root_init(realloc, free);
 }
 
 nvf_err nvf_parse_buf(const char *data, uintptr_t data_len, nvf_root *out_root) {
@@ -47,9 +42,82 @@ nvf_err nvf_parse_buf(const char *data, uintptr_t data_len, nvf_root *out_root) 
 			continue;
 		}
 
+		const char *name = &data[d_i];
+		while (!isspace(data[d_i]) && d_i < data_len) {
+			d_i++;
+		}
+		if (data_len >= d_i){
+			return NVF_BUF_OVF;
+		}
+		uintptr_t name_len = (data + d_i) - name;
+
+		while (isspace(data[d_i]) && d_i < data_len) {
+			d_i++;
+		}
+
+		// We've found value. Parse it depending on what it is.
+		const char *value = &data[d_i];
+		if (data[d_i] == '-' || isdigit(data[d_i])){
+			
+			// It's a number.
+			bool is_float = false;
+			for (; isdigit(data[d_i]) || data[d_i] == '.'; ++d_i) {
+				if (data[d_i] == '.') {
+					if (is_float) {
+						// That's a badly formatted floating point number..
+						return NVF_BAD_VALUE_FMT:
+					}
+					is_float = true;
+				}
+			}
+			uintptr_t value_len = (data + d_i) - value;
+			if (is_float) {
+
+			} else {
+				char *end; 
+				int64_t bin_value = strtoll(value, &end, 10);
+				if (bin_value == LLONG_MAX || bin_value == LLONG_MIN) {
+					return NVF_NUM_OVF;
+				}
+				if (value == end) {
+					// The integer didn't parse.
+					return NVF_BAD_VALUE_FMT;
+				}
+
+				// Grow the current map if we need to.
+				if (out_root->name_num + 1 > out_root->name_cap) {
+					nvf_num next_cap = out_root->cap*2 + 8;
+					char **new_names = out_root->realloc_inst(out_root->names, next_cap * sizeof(*new_names));
+					if (new_names == NULL) {
+						return NVF_BAD_ALLOC;
+					}
+					out_root->names = new_names;
+
+					uint8_t *new_types = out_root->realloc_inst(out_root->value_types, next_cap * sizeof(out_root->value_types));
+					if (new_types == NULL) {
+						return NVF_BAD_ALLOC;
+					}
+					out_root->types = new_types;
+					out_root->name_cap = next_cap;
+				}
+				if (out_root->value_num + 1 > out_root->value_cap) {
+					nvf_num next_cap = out_root->value_cap*2 + 8;
+					nvf_value *new_values = out_root->realloc_inst(out_root->values, next_cap * sizeof(*out_root->values));
+					if (new_values == NULL) {
+						return NVF_BAD_ALLOC;
+					}
+					out_root->values = new_values;
+					out_root->value_cap = next_cap;
+				}
+				out_root->values[out_root->value_num].v_int = bin_value;
+				out_root->value_num++;
+			}
+
+
+			
+		}
 
 	}
-	
 
 	return NVF_OK;
 }
--- nvf.h
+++ nvf.h
@@ -3,46 +3,49 @@
 #include <stddef.h>
 
 typedef enum {
-	NVF_OK,
+	NVF_OK = 0,
 	NVF_ERROR,
-	NVF_ALLOC_FAIL,
+	NVF_BAD_ALLOC,
+	NVF_BUF_OVF,
+	NVF_BAD_VALUE_FMT,
+	NVF_NUM_OVF,
 	NVF_NUM_ERRS,
 } nvf_err;
 
 typedef enum {
-	NVF_FLOAT,
+	NVF_FLOAT = 0,
 	NVF_INT,
 	NVF_BLOB,
 	NVF_STRING,
 	NVF_MAP,
 	NVF_ARRAY,
+	NVF_NUM_TYPES,
 } nvf_data_type;
+
 
 typedef union nvf_value {
 	int64_t v_int;
 	double v_float;
 	char *v_string;
+	uint8_t *v_blob;
 } nvf_value;
 
+// This is an index into the array of names..
+typedef uint32_t nvf_name_i;
+// This is used to keep track of how many elements are in something
+typedef uint32_t nvf_num;
+
 typedef struct nvf_array {
-	uintptr_t num;
-	// I can't think of a way to nest maps in an array.
-	// Don't allow that for now.
-	union { 
-		nvf_value val;
-		struct nvf_array *array;
-	} data[];
+	nvf_num   num, 
+		  cap;
+	nvf_value *values;
 } nvf_array;
 
 // TODO: Try to find a more efficient memory layout. I'm fairly sure we could make this better.
 typedef struct nvf_map {
-	uintptr_t num;
-	char **keys;
-	union {
-		nvf_value val;
-		struct nvf_map *map;
-		struct nvf_array *array;
-	} *values;
+	nvf_num num,
+		cap;
+	nvf_name_i *value_name_is;
 } nvf_map;
 
 typedef void* (*realloc_fn)(void *, size_t);
@@ -54,9 +57,23 @@ typedef void (*free_fn)(void *);
 typedef struct nvf_root {
 	realloc_fn realloc_inst;
 	free_fn free_inst;
-	nvf_map base_map;
+	nvf_num name_num,
+	        name_cap; 
+	uint8_t *value_types;
+	char **names;
+	nvf_num value_num,
+	        value_cap; 
+	nvf_value *values;
+
+	nvf_num array_num,
+	        array_cap; 
+	nvf_array *arrays;
+
+	nvf_num map_num,
+	        map_cap; 
+	nvf_map *maps;
 } nvf_root;
 
 nvf_root nvf_root_init(realloc_fn realloc_inst, free_fn free_inst);
 
-nvf_root nvf_alloc_default_init(void);
+nvf_root nvf_root_default_init(void);
