615a97e87dbbbf76d62809e19f1843f86d366d78
Aaron Klapatch <klapatchaaron@gmail.com>
2024-05-28 03:03:58 +0000

    Changed the testing style

--- nvf_test.c
+++ nvf_test.c
@@ -3,59 +3,41 @@
 #include <string.h>
 #include <stdio.h>
 
+#define ASSERT_INT(r, exp, ret_val, label)\
+	do {\
+		int _tmp_r = (r);\
+		int _tmp_exp = (exp);\
+		if (_tmp_r != _tmp_exp) {\
+			printf("%s failed. Expected %d (%s), got %d (%s)!\n", label, exp, #exp, r, #r);\
+			return ret_val;\
+		}\
+	} while (0)
+
 int main(int argc, char *argv[]){
-	int num_tests = 0;
-	int passed_tests = 0;
 	nvf_root root = {0};
 
 	const char int_test[] = "i_name 32343\nf_name 3.423";
 
-	num_tests++;
 	nvf_err rc = nvf_parse_buf(int_test, strlen(int_test), &root);
-	if (rc != NVF_NOT_INIT){
-		printf("Init should have failed! rc=%u\n", rc);
-	} else {
-		passed_tests++;
-	}
+	ASSERT_INT(rc, NVF_NOT_INIT, 1, "Testing init failure");
 
 	root = nvf_root_default_init();
 
-	num_tests++;
 	rc = nvf_parse_buf(int_test, strlen(int_test), &root);
-	if (rc != NVF_OK){
-		printf("Parsing an int failed! rc=%u\n", rc);
-	} else {
-		passed_tests++;
-	}
+	ASSERT_INT(rc, NVF_OK, 1, "Parsing data");
 
 	int64_t bin_int = 0;
-	++num_tests;
 	const char *names[] = {"i_name"};
 	rc = nvf_get_int(&root, names, 1, &bin_int);
-	if (rc != NVF_OK){
-		printf("Getting the int failed! rc=%u\n", rc);
-	} else {
-		passed_tests++;
-	}
+	ASSERT_INT(rc, NVF_OK, 1, "Getting an int");
 
-	++num_tests;
-	int64_t exp_val = 32343;
-	if (bin_int != exp_val) {
-		printf("Value %ld didn't match expected %ld\n", bin_int, exp_val);
-	} else {
-		passed_tests++;
-	}
+	int exp_val = 32343;
+	ASSERT_INT((int)bin_int, exp_val, 1, "Comparing int values");
 
-
-	num_tests++;
 	rc = nvf_deinit(&root);
+	ASSERT_INT(rc, NVF_OK, 1, "Deiniting the root");
 	// TODO: Add a test that the root is zeroed.
-	if (rc != NVF_OK){
-		printf("Deiniting the root failed! rc=%u\n", rc);
-	} else {
-		passed_tests++;
-	}
 
-	printf("%d/%d tests passed\n", passed_tests, num_tests);
+	printf("All tests passed.\n");
 	return 0;
 }
